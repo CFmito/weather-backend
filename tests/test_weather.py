2c6275bd4a275f407bcf793991e628fd2712212c
Akira Kurizawa <akirakurizawa@gmail.com>
2026-09-05 12:10:31 +0500

    test correct

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,14 +1,12 @@
 import pytest
 from fastapi.testclient import TestClient
-from app.main import app  # Если главный файл FastAPI находится в app/main.py
+from main import app  # Заменили app.main на main
 
 client = TestClient(app)
 
 def test_health_check():
-    """Базовый тест для проверки, что тест-фреймворк работает"""
     assert True
 
 def test_read_main():
-    """Тест проверки доступности корневого эндпоинта или документации"""
     response = client.get("/docs")
     assert response.status_code == 200
